ff79c89c2eb83174e3ba7ff154fe14bf98adfed1
jAnspoh <91568736+jAnspoh@users.noreply.github.com>
2022-01-23 21:59:59 +0100

    Update plot.py

--- plot.py
+++ plot.py
@@ -13,7 +13,9 @@ def plot(X ,Y ,title ,num=5 ,top=False):
     for n in sorted_data[:num]:
         names.append(X[Y.index(n)])
     plt.figure(figsize=(10,5))
-    if top: plt.bar(names ,sorted_data[:num] ,color="green")
-    else: plt.bar(names ,sorted_data[:num] ,color="green")
+    if top: 
+        plt.bar(names ,sorted_data[:num] ,color="green")
+    else: 
+        plt.bar(names ,sorted_data[:num] ,color="green")
     plt.title(title ,fontsize=15)
     plt.show()
